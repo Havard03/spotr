ff4d3a3497664141fd13190bc0e5ba3a4fefbe66
Håvard <havard.buvang@gmail.com>
2023-03-26 22:05:39 +0200

    Pylint+Imporvements+Blackformatter+Yarl

--- API.py
+++ API.py
@@ -1,145 +1,200 @@
-from dotenv import load_dotenv, find_dotenv
+""" API class """
+
+import base64
+import logging
+import os
+import sys
+import time
+import webbrowser
+
+import requests
+from dotenv import find_dotenv, load_dotenv
 from rich.logging import RichHandler
-import subprocess, webbrowser, base64, os, requests, time, logging
 
 load_dotenv(find_dotenv())
-log = logging.getLogger();
+log = logging.getLogger()
 
-if (os.environ["debug"] == "True"):
-    logging.basicConfig(level="NOTSET", format="%(message)s", datefmt="[%X]", handlers=[RichHandler(markup=True, rich_tracebacks=True)])
-else: 
-    logging.basicConfig(level="INFO", datefmt="[%X]", format="%(message)s", handlers=[RichHandler(markup=True)])
+if bool(os.environ["DEBUG"]):
+    logging.basicConfig(
+        level="NOTSET",
+        format="%(message)s",
+        datefmt="[%X]",
+        handlers=[RichHandler(markup=True, rich_tracebacks=True)],
+    )
+else:
+    logging.basicConfig(
+        level="INFO",
+        datefmt="[%X]",
+        format="%(message)s",
+        handlers=[RichHandler(markup=True)],
+    )
 
-class API():
+
+class API:
+    """API class for seding all requests"""
 
     def __init__(self):
         try:
             self.path = os.environ["project_path"]
             self.refresh_token = os.environ["refresh_token"]
             self.base_64 = os.environ["base_64"]
-            with open(os.environ["project_path"]+"key.txt") as file: self.TOKEN = file.read()
+            with open(os.environ["project_path"] + "key.txt", encoding="utf-8") as file:
+                self.TOKEN = file.read()
         except KeyError:
             log.critical("[bold red]Enviorment-Variables are not set!")
-            log.exception("[bold blue]Try running the authorise command") if os.environ["debug"] == "True" else log.info("[bold blue]Try running the authorise command")
-            pass
+            DEBUG: bool = (
+                log.exception("[bold blue]Try running the authorise command")
+                if bool(os.environ["DEBUG"])
+                else log.info("[bold blue]Try running the authorise command")
+            )
 
-    def request(self, type, url, headers=None, json=None):
-        if headers is None: headers = {"Authorization": f"Bearer {self.TOKEN}"}
-
-        response = requests.request(type, url, headers=headers, json=json)
-        
-        if (response.status_code == 401 or response.status_code == 400):
-            self.refresh_key()
-            with open(os.environ["project_path"]+"key.txt") as file: self.TOKEN = file.read()
+    def request(self, method, url, headers=None, json=None):
+        """Spotr request, with deafult headers"""
+        if headers is None:
             headers = {"Authorization": f"Bearer {self.TOKEN}"}
-            response = requests.request(type, url, headers=headers, json=json)
 
-        if (not response.ok):
-            log.warning(f"[bold red]request error | status-code: {response.status_code}")
+        response = requests.request(method, url, headers=headers, json=json, timeout=10)
+
+        if response.status_code in (401, 400):
+            self.refresh_key()
+            with open(os.environ["project_path"] + "key.txt", encoding="utf-8") as file:
+                self.TOKEN = file.read()
+            headers = {"Authorization": f"Bearer {self.TOKEN}"}
+            response = requests.request(
+                method, url, headers=headers, json=json, timeout=10
+            )
+
+        if not response.ok:
+            log.warning(
+                "[bold red]request error | status-code: %d", response.status_code
+            )
             log.info(response.json())
-            exit()
+            sys.exit()
 
         try:
             response.json()
         except ValueError:
-            return
-        else:
-            return response.json()
+            return None
+
+        return response.json()
 
     def refresh_key(self):
+        """Refresh API key"""
         URL = "https://accounts.spotify.com/api/token"
 
-        response = requests.post(URL,
+        response = requests.post(
+            URL,
             data={"grant_type": "refresh_token", "refresh_token": self.refresh_token},
-            headers={"Authorization": "Basic " + self.base_64}
+            headers={"Authorization": "Basic " + self.base_64},
+            timeout=10,
         )
 
-        if (not response.ok): 
-            log.warning(f"[bold red]request error | status-code: {response.status_code}")
-            log.info("[bold blue]Most likely something wrong with base_64 or refresh_token, try running [bold green]spotr authorise[/bold green]")
-            exit()
+        if not response.ok:
+            log.warning(
+                "[bold red]request error | status-code: %d",
+                response.status_code,
+            )
+            log.info(
+                "[bold blue]Most likely something wrong with base_64 or refresh_token, try running [bold green]spotr authorise[/bold green]"
+            )
+            sys.exit()
 
         data = response.json()
 
-        with open(self.path + "key.txt", "w") as f:
+        with open(self.path + "key.txt", "w", encoding="utf-8") as f:
             f.write(data["access_token"])
             f.close()
-            
-        return
 
     def authorise(self):
-        AUTH_URL = 'https://accounts.spotify.com/authorize'
-        TOKEN_URL = 'https://accounts.spotify.com/api/token'
-        
-        CLIENT_ID = str(input("Client id: "))
-        CLIENT_SECRET = str(input("Client secret: "))
-                            
-        auth_request = requests.get(AUTH_URL, {
-            'client_id': CLIENT_ID,
-            'response_type': 'code',
-            'redirect_uri': 'https://www.google.com/',
-            'scope': 'playlist-read-collaborative playlist-read-private user-read-playback-state user-modify-playback-state user-read-currently-playing user-read-recently-played playlist-modify-private playlist-modify-public',
-        })
+        """Authtication process, Get BASE_64 and REFRESH_KEY"""
+        auth_url = "https://accounts.spotify.com/authorize"
+        token_url = "https://accounts.spotify.com/api/token"
 
-        log.info(f'URL will open in 5 seconds, Accept the terms, Copy the code in the redirected URL, Then paste the code into the terminal')
+        client_id = str(input("Client id: "))
+        client_secret = str(input("Client secret: "))
+
+        auth_request = requests.get(
+            auth_url,
+            {
+                "client_id": client_id,
+                "response_type": "code",
+                "redirect_uri": "https://www.google.com/",
+                "scope": "playlist-read-collaborative playlist-read-private user-read-playback-state user-modify-playback-state user-read-currently-playing user-read-recently-played playlist-modify-private playlist-modify-public",
+            },
+            timeout=10,
+        )
+
+        log.info(
+            "URL will open in 5 seconds, Accept the terms, Copy the code in the redirected URL, Then paste the code into the terminal"
+        )
         time.sleep(5)
         webbrowser.open_new_tab(auth_request.url)
-        
+
         auth_code = str(input("Enter code from the URL: "))
-        
-        client_creds = f"{CLIENT_ID}:{CLIENT_SECRET}"
+
+        client_creds = f"{client_id}:{client_secret}"
         client_creds_b64 = base64.b64encode(client_creds.encode())
-        
+
         headers = {
             "Content-Type": "application/x-www-form-urlencoded",
-            "Authorization": "Basic %s" % client_creds_b64.decode()
+            "Authorization": "Basic %s" % client_creds_b64.decode(),
         }
         payload = {
             "grant_type": "authorization_code",
             "code": auth_code,
             "redirect_uri": "https://www.google.com/",
         }
-        
-        access_token_request = requests.post(url=TOKEN_URL, data=payload, headers=headers)
 
-        if (not access_token_request.ok):
-            log.warning(f"Request error: {access_token_request.status_code}")
-            exit()
+        access_token_request = requests.post(
+            url=token_url, data=payload, headers=headers, timeout=10
+        )
+
+        if not access_token_request.ok:
+            log.warning("Request error: %d", access_token_request.status_code)
+            sys.exit()
 
         access_token_response_data = access_token_request.json()
 
         try:
-            self.path
-
             env_data = f"""
-            #Enviorment-variables
+            #Environmental-variables
             project_path="{self.path}"
             refresh_token="{access_token_response_data['refresh_token']}"
             base_64="{client_creds_b64.decode()}"
-            debug="False"
-            """.replace(" ",  "")
+            DEBUG="False"
+            """.replace(
+                " ", ""
+            )
 
-            with open(self.path + ".env", "w") as f:
+            with open(self.path + ".env", "w", encoding="utf-8") as f:
                 f.write(env_data)
 
-        except:
-            PATH = os.path.dirname(__file__)
+        except ValueError:
+            path = os.path.dirname(__file__)
 
-            check_path = str(input("Data will be written to the following PATH, is it correct? (" + PATH + ") y/n: "))
+            check_path = str(
+                input(
+                    "Data will be written to the following PATH, is it correct? ("
+                    + path
+                    + ") y/n: "
+                )
+            )
             if check_path.lower() != "y":
-                exit()
+                sys.exit()
 
-            if not os.path.exists(PATH + "/.env") or os.path.exists(PATH + "/key.txt"):
-                open(PATH + '/key.txt', "w").close()
-                open(PATH + '/.env', "w").close()
+            if not os.path.exists(path + "/.env") or os.path.exists(path + "/key.txt"):
+                open(path + "/key.txt", "w", encoding="utf-8").close()
+                open(path + "/.env", "w", encoding="utf-8").close()
 
             env_data = f"""
-            #Enviorment-variables
-            project_path="{PATH}/"
+            #Environmental-variables
+            project_path="{path}/"
             refresh_token="{access_token_response_data["refresh_token"]}"
             base_64="{client_creds_b64.decode()}"
-            debug="False"
-            """.replace(" ",  "")
+            DEBUG="False"
+            """.replace(
+                " ", ""
+            )
 
-            with open(PATH + "/.env", "w") as f:
+            with open(path + "/.env", "w", encoding="utf-8") as f:
                 f.write(env_data)
--- install.py
+++ install.py
@@ -1,51 +1,58 @@
+""" Install script for spotr """
+
 import os
+import sys
 import subprocess
 
-#Get current path
+# Get current path
 PATH = os.path.dirname(__file__)
 
-check_path = str(input("Data will be written to the following PATH, is it correct? (" + PATH + ") y/n: "))
-if check_path.lower() != "y":
-    exit()
+CHECK_PATH = str(
+    input(
+        "Data will be written to the following PATH, is it correct? ("
+        + PATH
+        + ") y/n: "
+    )
+)
+if CHECK_PATH.lower() != "y":
+    sys.exit()
 
-#check if .env and key.txt exists and creates them if not
+# check if .env and key.txt exists and creates them if not
 print("Creating .env and key.txt files...")
-if not os.path.exists(PATH + '/.env'):
-    open(PATH + '/.env', "w").close()
-if not os.path.exists(PATH + '/key.txt'):
-    open(PATH + '/key.txt', "w").close()
-    
-#make spot file executable
+if not os.path.exists(PATH + "/.env"):
+    open(PATH + "/.env", "w", encoding='utf-8').close()
+if not os.path.exists(PATH + "/key.txt"):
+    open(PATH + "/key.txt", "w", encoding='utf-8').close()
+
+# make spot file executable
 print("Making spot file executable...")
-subprocess.run(['chmod','u+x', PATH+"/spotr"])
+subprocess.run(["chmod", "u+x", PATH + "/spotr"], check = True)
 
-#install requirements
+# install requirements
 print("Installing spot requirements...")
-subprocess.run(['pip3', 'install',  '-r', PATH + "/requirements.txt"])
+subprocess.run(["pip3", "install", "-r", PATH + "/requirements.txt"], check = True)
 
-#write template data to .env (Needed to run spot auth)
+# write template data to .env (Needed to run spot auth)
 print("Writing template data to .env...")
 env_data = f"""
-#Enviorment-variables
+#Environmental-variables
 project_path="{PATH}/"
 refresh_token=""
 base_64=""
-debug="False"
-""".replace(" ", "")
-with open(PATH + '/.env', 'w') as f:
+DEBUG="False"
+""".replace(
+    " ", ""
+)
+with open(PATH + "/.env", "w", encoding='utf-8') as f:
     f.write(env_data)
 
-#start authetication process
+# start authetication process
 print("Starting spotify authentication process...")
-subprocess.run(['./spotr', 'authorise'])
+subprocess.run(["./spotr", "authorise"], check = True)
 
-from rich.console import Console
-console = Console()
-
-console.log("[bold blue]Add this folder to your PATH if you wish to execute spot commands anywhere")
-console.log(f"[bold blue]This can be adding the following to your .rc file ([bold white]export PATH=$PATH:{PATH}[/bold white])")
-
-
-
-
-    
+print(
+    "---Add this folder to your PATH if you wish to execute spot commands anywhere---"
+)
+print(
+    f"---This can be adding the following to your .rc file ([bold white]export PATH=$PATH:{PATH}---)"
+)
